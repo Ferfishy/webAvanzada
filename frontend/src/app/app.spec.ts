301803fbcf81c43e6073e7cfc75cc9aed386b7c1
Ferfishy <fenanofig@gmail.com>
2026-09-07 10:55:14 -0300

    test: provoca fallo controlado de CI

--- frontend/src/app/app.spec.ts
+++ frontend/src/app/app.spec.ts
@@ -14,6 +14,6 @@ describe('App', () => {
  fixture.detectChanges();
  const compiled = fixture.nativeElement as HTMLElement;
  expect(compiled.querySelector('h1')?.textContent)
- .toContain('Catálogo de Recursos');
+ .toContain('Título incorrecto');
  });
 });
